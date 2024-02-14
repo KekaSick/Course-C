047aa2703c7e5b33ff5dca8cbb7a0c29afc36815
KekaSick <mlggg@bk.ru>
2024-02-14 18:32:20 +0300

    OLD SEMINARS

---
+++ build/CMakeFiles/problem8_filter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem8_filter.dir/problem8_filter/main.cpp.o"
+  "CMakeFiles/problem8_filter.dir/problem8_filter/main.cpp.o.d"
+  "problem8_filter"
+  "problem8_filter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problem8_filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
