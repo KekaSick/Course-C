d46e435c23fea75c2f2ffb67f1a0b6145b9c11db
KekaSick <mlggg@bk.ru>
2024-02-26 19:32:06 +0300

    WATA HELL

---
+++ week7/13-14_sem/build/CMakeFiles/problem2_time.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem2_time.dir/problem2_time/main.cpp.o"
+  "CMakeFiles/problem2_time.dir/problem2_time/main.cpp.o.d"
+  "problem2_time"
+  "problem2_time.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problem2_time.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week7/13-14_sem/build/CMakeFiles/problem3_flight.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for problem3_flight.
+# This may be replaced when dependencies are built.
